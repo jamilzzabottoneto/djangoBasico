b658bdd3d1b40aad128b1f8200e26d0fe2529d66
jamilzzabottoneto <jamilzabneto@hotmail.com>
2025-03-27 12:30:53 -0300

    Adicionado a url comentarios no projeto

--- djangoBasico/apps/blog/views.py
+++ djangoBasico/apps/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def coments(request):
+    return render(request, 'blog/comentarios.html')
